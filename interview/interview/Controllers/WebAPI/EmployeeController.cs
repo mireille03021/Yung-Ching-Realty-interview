2a8b18d61daf69e6db3bf8fce6c14d4196bc3841
mireille03021 <mireille03021@gmail.com>
2022-07-13 19:09:43 +0800

    調整畫面改串 Web Api 做 CRUD

--- interview/interview/Controllers/WebAPI/EmployeeController.cs
+++ interview/interview/Controllers/WebAPI/EmployeeController.cs
@@ -1,7 +1,10 @@
 ﻿using interview.Data;
 using interview.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,29 +16,36 @@ namespace interview.Controllers.WebAPI
     {
         private IEmployeesRepository _employeeRepository;
 
-        public EmployeeController(IEmployeesRepository employeesRepository)
+        public EmployeeController(IEmployeesRepository employeesRepository, IConfiguration configuration)
         {
             this._employeeRepository = employeesRepository;
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
         }
 
-        // GET api/Employee/{id}
-        [HttpGet("{id}")]
+        // GET api/Employee/Get/{id}
+        [HttpGet("Get/{id}")]
         public string Get(int id)
         {
             var employee = this._employeeRepository.getById(id);
             return JsonConvert.SerializeObject(employee);
         }
 
+        [HttpGet("GetList")]
+        public List<Employee> getList()
+        {
+            return this._employeeRepository.get();
+        }
+
         // POST api/Employee
         [HttpPost]
-        public bool Post(Employee employee)
+        public bool CreateEmployee(Employee employee)
         {
             return this._employeeRepository.create(employee);
         }
 
         // PUT api/Employee/{id}
         [HttpPut("{id}")]
-        public bool Put(int id,Employee employee)
+        public bool UpdateEmployee(int id,Employee employee)
         {
             return this._employeeRepository.update(id,employee);
         }
--- interview/interview/Repository/IEmployeesRepository.cs
+++ interview/interview/Repository/IEmployeesRepository.cs
@@ -1,4 +1,5 @@
 ﻿using interview.Data;
+using System.Collections.Generic;
 
 namespace interview.Repository
 {
@@ -8,13 +9,17 @@ namespace interview.Repository
         //新增
         public bool create(Employee employee);
 
-        //刪除
+        //刪除 By Id
         public bool deleteById(int id);
 
         //修改
         public bool update(int id,Employee employee);
 
-        //查尋
+        //查尋 By Id
         public Employee getById(int id);
+
+        //查詢
+        public List<Employee> get();
+
     }
 }
